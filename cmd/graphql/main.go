dd370344c74b15a04e821d09d148801d7a675042
friasdesign <carlos.a.frias@gmail.com>
2018-03-14 10:20:12 -0300

    Add authenticator

--- cmd/graphql/main.go
+++ cmd/graphql/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type body struct {
+	Message string `json:"message"`
+}
+
 // Handler is used by AWS Lambda to handle request.
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Something")
+
+	bStr, _ := json.Marshal(body{
+		Message: "Everything OK!",
+	})
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
+		Body:       string(bStr),
 	}, nil
 }
 
